1d63ad3cd2833a4d9adadbce9107ac6e6a06878a
Xin Wang <wangxin94@163.com>
2017-04-26 21:00:48 -0400

    can load file into lists

--- code/preprocessing.py
+++ code/preprocessing.py
@@ -11,14 +11,19 @@ def load_dataset(filename, separator):
     with open(filename, 'r') as training_data:
         print 'in load_dataset'
         for line in training_data:
-            line = line.split(separator)
-            for l in line:
-                if not l.strip():
-                    continue
+            line = line.split(separator)[:3]
+            if has_empty(line):
+                continue
+            else:
                 print line
                 userid_list.append(line[0])
                 movieid_list.append(line[1])
                 ratings_list.append(line[2])
+
+def has_empty(line):
+    for l in line:
+        if not l.strip():
+            return False
 
 def main():
     training_file = os.path.join(data_dir, file_name)
