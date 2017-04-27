06ff4c7c4601c6fcc6338789f6e51c76cfca00fb
Xin Wang <wangxin94@163.com>
2017-04-26 20:54:53 -0400

    can read line but throw empty rows are not correct

---
+++ code/hw9_test.py
@@ -0,0 +1,4 @@
+# check if the string is empty
+print not "    "
+
+print not "    ".strip()
---
+++ code/preprocessing.py
@@ -0,0 +1,30 @@
+import os
+import time
+
+data_dir = os.path.join('../', 'RSdata/')
+file_name = 'training_rating.dat'
+userid_list = []
+movieid_list = []
+ratings_list = []
+
+def load_dataset(filename, separator):
+    with open(filename, 'r') as training_data:
+        print 'in load_dataset'
+        for line in training_data:
+            line = line.split(separator)
+            for l in line:
+                if not l.strip():
+                    continue
+                print line
+                userid_list.append(line[0])
+                movieid_list.append(line[1])
+                ratings_list.append(line[2])
+
+def main():
+    training_file = os.path.join(data_dir, file_name)
+    load_dataset(training_file, "::")
+    print "len(user_id) = %d, len(movieid_list) = %d, len(ratings_list) = %d" % (len(userid_list), len(movieid_list), len(ratings_list))
+
+start_time = time.time()
+main()
+print("--- %s seconds ---" % (time.time() - start_time))
