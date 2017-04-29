11fe8345888df97bd074ca90affdb968ac3c9960
Xin Wang <wangxin94@163.com>
2017-04-29 16:36:38 -0400

    change to coo_matrix

--- code/preprocess.py
+++ code/preprocess.py
@@ -1,27 +1,34 @@
 import os
 import time
+from scipy.sparse import coo_matrix
 
 # TODO: build matrix
 data_dir = os.path.join('../', 'RSdata/')
 file_name = 'training_rating.dat'
-userid_list = []
-movieid_list = []
-ratings_list = []
+
 
 def load_dataset(filename, separator):
     with open(filename, 'r') as training_data:
         count = 0
+        userid_list = []
+        movieid_list = []
+        ratings_list = []
         for line in training_data:
             count += 1
             line = line.rstrip('\n').split(separator)[:3]
             if has_empty(line):
                 continue
             else:
-                #print line
-                userid_list.append(line[0])
-                movieid_list.append(line[1])
-                ratings_list.append(line[2])
+                # print line
+                userid_list.append(int(line[0]))
+                movieid_list.append(int(line[1]))
+                ratings_list.append(int(line[2]))
+        num_users = max(userid_list) + 1
+        num_movies = max(movieid_list) + 1
+        matrix_coo = coo_matrix((ratings_list, (userid_list, movieid_list)),
+    					shape=(num_users, num_movies), dtype='float32')
         print 'line numbers: ', count
+        return matrix_coo, userid_list, movieid_list, ratings_list
 
 def has_empty(line):
     for l in line:
@@ -30,9 +37,10 @@ def has_empty(line):
 
 def main():
     training_file = os.path.join(data_dir, file_name)
-    load_dataset(training_file, "::")
+    (matrix_coo, userid_list, movieid_list, ratings_list) = load_dataset(training_file, "::")
     #print has_empty(['', '  ', 'd'])
     print "len(user_id) = %d, len(movieid_list) = %d, len(ratings_list) = %d" % (len(userid_list), len(movieid_list), len(ratings_list))
+    print 'matrix_coo shape: ', matrix_coo.shape
 
 start_time = time.time()
 main()
