92b1d88636a9e1be6ebd30da5bdea1ecdf293e16
Xin Wang <wangxin94@163.com>
2017-05-01 10:54:39 -0400

    load and save test

--- code/hw9_test.py
+++ code/hw9_test.py
@@ -1,5 +1,10 @@
-import preprocess
+import numpy as np
 # check if the string is empty
 # print not "    "
 #
 # print not "    ".strip()
+lambdaU_out = "lambdaU.npy"
+x = np.arange(10)
+np.save(lambdaU_out, x)
+y = np.load(lambdaU_out)
+print y
