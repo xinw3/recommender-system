13bed2b86b2ec8863ee80fe5683d257ad35f0e95
Xin Wang <wangxin94@163.com>
2017-05-02 13:13:18 -0400

    rmse drop down much when change D from 5 to 10

--- code/new_pmf.py
+++ code/new_pmf.py
@@ -18,7 +18,7 @@ output_file = os.path.join(data_dir, "result.csv")
 '''
 Tunable parameters
 '''
-D = 5             #number of factors [1:20]
+D = 10             #number of factors [1:20]
 eta = 0.01         #learning rate
 lambdaU = 0.1
 lambdaV = 0.1
