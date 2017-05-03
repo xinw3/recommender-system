1b342a20d0135c04f3e3221d0192d29c9997b8e2
Cloud User <ec2-user@ip-172-31-59-244.ec2.internal>
2017-05-03 12:36:55 -0400

    cleanup

--- code/new_pmf.py
+++ code/new_pmf.py
@@ -14,15 +14,15 @@ training_file = os.path.join(data_dir, "training_rating.dat")
 test_file = os.path.join(data_dir, "testing.dat")
 output_file = os.path.join('./', "results.csv")
 
-
 '''
 Tunable parameters
 '''
-D = 7             #number of factors [1:20]
-lambdaU = 0.4
-lambdaV = 0.4
+D = 10             
+lambdaU = 0.1
+lambdaV = 0.1
 maxRating = 5
 training_iterations = 20
+valid_percentage = 0.05
 
 def preprocess_test_file(test_file):
     movieid_list = []
@@ -51,8 +51,7 @@ def preprocess_training_file(training_data):
     return userid_list, movieid_list, rating_list
 
 # Removes bad lines from training file and
-# causes split such that 95% data is training
-# and 5% data is for validation
+# causes split into training and testing
 def split_training_data(original_training_file):
     training_list = []
     validation_data = []
@@ -63,9 +62,7 @@ def split_training_data(original_training_file):
                 continue
             training_list.append(line)
     training_data.close()
-    validation_indices =  random.sample(range(0, len(training_list)), int(0.05*len(training_list)))
-    pickle.dump(validation_indices, open("validation_indices", "wb"))
-    #validation_indices = pickle.load(open("validation_indices", "rb"))
+    validation_indices =  random.sample(range(0, len(training_list)), int(valid_percentage*len(training_list)))
     for index in validation_indices:
         validation_data.append(training_list[index])
     training_data = np.reshape(training_list, (1, len(training_list)))
@@ -73,13 +70,6 @@ def split_training_data(original_training_file):
     training_data = list(training_data)
     return training_data, validation_data
 
-'''
-Build dictionaries for user-movie ratings
-key: user_id
-value: (dict)
-    key: movie_id the user has rated
-    value: ratings
-'''
 def get_users_per_movie_and_vice_versa(userid_list, movieid_list):
     users_per_movie = dict()
     movies_per_user = dict()
@@ -111,6 +101,13 @@ def get_users_per_movie_and_vice_versa(userid_list, movieid_list):
 
     return users_per_movie, movies_per_user
 
+'''
+Build dictionaries for user-movie ratings
+key: user_id
+value: (dict)
+    key: movie_id the user has rated
+    value: ratings
+'''
 def get_dictionaries(userid_list, movieid_list, rating_list):
     number_users = max(userid_list)
     number_movies = max(movieid_list)
@@ -135,45 +132,17 @@ def has_empty(elements):
             return True
     return False
 
-# Normalises ratings to [0,1] scale
-def normalize_ratings(ratings):
-    '''
-    Input: ratings: ratings list
-                 K: The upper bound of ratings
-    Output: normalized value of ratings.[0, 1]
-    '''
-    K = maxRating
-    for i in range(0, len(ratings)):
-        ratings[i] = float((ratings[i] - 1)) / (K - 1)
-    return ratings
-
-def nonnormalize_ratings(ratings):
-    for i in range(0, ratings.shape[0]):
-        for j in range (0, ratings.shape[1]):
-            if (ratings[i][j] > 5):
-                    ratings[i][j] = 5
-            if (ratings[i][j] < 1):
-                ratings[i][j] = 1
-    return ratings
-
-def loss(U, V, ratings_matrix, w_matrix):
-    product_matrix = U.T.dot(V)
-    difference_matrix = ratings_matrix - product_matrix
-    actual_difference_matrix = np.multiply(w_matrix, difference_matrix)
-    square_matrix = np.square(actual_difference_matrix)
-    print square_matrix.shape
-
-    loss = np.sum(square_matrix)
-    loss = loss +  (lambdaU  * (LA.norm(U, 'fro')))
-    loss = loss + (lambdaV * (LA.norm(V, 'fro')))
-
-
 def RMSE(predicts, actual):
     rmse = 0.0
     counter = 0
     for user in actual:
     	for movie in actual[user]:
-    	    rmse = rmse + (actual[user][movie]-predicts[user - 1][movie - 1])**2
+            prediction = predicts[user - 1][movie -1]
+            if (prediction < 1.0):
+                prediction = 1.0
+            elif (prediction > 5.0):
+                prediction = 5.0       
+    	    rmse = rmse + (actual[user][movie]-prediction)**2
             counter = counter + 1
     rmse = (rmse * 1.0/counter) ** 0.5
     return rmse
@@ -181,12 +150,9 @@ def RMSE(predicts, actual):
 
 def ALS(U, V, ratings_matrix, users_per_movie, movies_per_user):
     # update U, latent vector U, fixed vector V
-    #VTV = (V.dot(w_movies)).dot(V.T)    # D * D
-    #lambdaU_matrix = np.eye(VTV.shape[0]) * lambdaU
     lambdaU_matrix = np.eye(D) * lambdaU
     for u in range(0, U.shape[1]):
         if  u+1 not in movies_per_user:
-            #print u+1
             continue
         V_sub = V[:, movies_per_user[u+1]]
         VTV = V_sub.dot(V_sub.T)
@@ -195,12 +161,9 @@ def ALS(U, V, ratings_matrix, users_per_movie, movies_per_user):
         U[:, u] = solve((VTV + lambdaU_matrix), second)
     
     # update V
-    #UTU = (U.dot(w_users)).dot(U.T)
-    #lambdaV_matrix = np.eye(UTU.shape[0]) * lambdaV
     lambdaV_matrix = np.eye(D) * lambdaV
     for v in range(0, V.shape[1]):
         if v+1 not in users_per_movie:
-            #print "Movie No one rated: ", v+1
             continue
         U_sub = U[:, users_per_movie[v+1]]
         UTU = U_sub.dot(U_sub.T)
@@ -226,71 +189,56 @@ def main():
 
     # (number_users, number_movies) (6040, 3883)
     ratings_matrix = np.zeros((number_users + 1, number_movies + 1))
-    w_matrix = np.zeros((number_users + 1, number_movies + 1))
     valid_ratings_matrix = np.zeros((valid_number_users + 1, valid_number_movies + 1))
-    w_valid_matrix = np.zeros((valid_number_users + 1, valid_number_movies + 1))
 
     # training_rating_matrix
     for userid in userMovieDict:
         for movieid in userMovieDict[userid]:
             ratings_matrix[userid][movieid] = userMovieDict[userid][movieid]
-            w_matrix[userid][movieid] = 1
 
     # valid_rating_matrix
     for userid in valid_user_movie_dict:
         for movieid in valid_user_movie_dict[userid]:
             valid_ratings_matrix[userid][movieid] = valid_user_movie_dict[userid][movieid]
-            w_valid_matrix[userid][movieid] = 1
 
     # complete_ratings_matrix: size (6040, 3883)
     ratings_matrix = np.delete(ratings_matrix, 0, 0)
     ratings_matrix = np.delete(ratings_matrix, 0, 1)
-    w_matrix = np.delete(w_matrix, 0, 0)
-    w_matrix = np.delete(w_matrix, 0, 1)
 
     # valid_ratings_matrix
     valid_ratings_matrix = np.delete(valid_ratings_matrix, 0, 0)
     valid_ratings_matrix = np.delete(valid_ratings_matrix, 0, 1)
-    w_valid_matrix = np.delete(w_valid_matrix, 0, 0)
-    w_valid_matrix = np.delete(w_valid_matrix, 0, 1)
 
     users_per_movie, movies_per_user = get_users_per_movie_and_vice_versa(training_userid_list, training_movieid_list)
-
-    # U (D, 6040), V(D, 3883)
-    U = np.random.rand(D, number_users)
-    V = np.random.rand(D, number_movies)
-
-    # U = pickle.load(open("U", "rb"))
-    # V = pickle.load(open("V", "rb"))
+    
+    averageRating = sum(training_rating_list) * 1.0/len(training_rating_list)
+ 
+    U = np.ones((D, number_users)) * averageRating
+    V = np.ones((D, number_movies)) * averageRating
 
     for i in range(0, training_iterations):
         U, V = ALS(U, V, ratings_matrix, users_per_movie, movies_per_user )
         predictions = U.T.dot(V)
-        #training_loss = loss(U, V, ratings_matrix, w_matrix)
         training_RMSE = RMSE(predictions, userMovieDict)
-
-        #valid_loss = loss(U, V, valid_ratings_matrix, w_valid_matrix)
         valid_RMSE = RMSE(predictions, valid_user_movie_dict)
         print '##### training iterations %d ####' % (i)
-        #print "Train Loss ", training_loss
         print "Train RMSE ", training_RMSE
-        #print "Valid Loss ", valid_loss
         print "Valid RMSE ", valid_RMSE
         print ""
-
+    
     #TESTING CODE FOLLOWS
     userid_list, movieid_list = preprocess_test_file(test_file)
     output = open(output_file, 'w')
-    U = pickle.load(open("U", "rb"))
-    V = pickle.load(open("V", "rb"))
     ratings = U.T.dot(V)
-    ratings = nonnormalize_ratings(ratings)
     for i in range(0, len(userid_list)):
        user = userid_list[i]
        movie = movieid_list[i]
-       output.write(str(ratings[user - 1][movie - 1]))
+       rating = ratings[user - 1][movie - 1]
+       if (rating < 1):
+           rating = 1.0
+       elif (rating > 5):
+           rating = 5.0
+       output.write(str(rating))
        output.write("\n")
     output.close()
-    #    print ratings[user - 1][movie - 1]
-
 main()
